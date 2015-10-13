4e0add6899e841c6a05a25dd74f788d9c7a586a2
Jason DeVita <jason@wx13.com>
2015-10-13 14:04:22 -0700

    include version number in sith logo
    	modified:   editor.go

--- editor.go
+++ editor.go
@@ -330,7 +330,7 @@ func (editor *Editor) UpdateStatus() {
 	)
 	col := cols - len(message)
 	editor.screen.WriteString(rows-1, col, message)
-	editor.screen.WriteString(rows-1, 0, "[ Sith ]")
+	editor.screen.WriteString(rows-1, 0, "[ Sith 0.1.2 ]")
 	editor.screen.DecorateStatusLine()
 	editor.file.WriteStatus(rows-1, col)
 	editor.screen.SetCursor(editor.file.GetCursor(0))
