6c58ddb4288816c51bbc775e5ccb2c7e94f6e4d9
Jason DeVita <jason@wx13.com>
2016-01-28 21:48:15 -0800

    cleaned up syntax color code and add some tests
    	modified:   syntaxcolor/syntaxcolor.go
    	new file:   syntaxcolor/syntaxcolor_test.go

--- syntaxcolor/syntaxcolor.go
+++ syntaxcolor/syntaxcolor.go
@@ -5,6 +5,7 @@ import "github.com/nsf/termbox-go"
 import "path"
 import "fmt"
 
+// Color specifies a foreground and background color.
 type Color struct {
 	fg, bg termbox.Attribute
 }
@@ -15,15 +16,20 @@ type SyntaxRule struct {
 	color Color
 }
 
+// SyntaxRules is the full collection of all syntax rules.
 type SyntaxRules []SyntaxRule
 
 func NewSyntaxRules(filename string) *SyntaxRules {
-
 	rules := SyntaxRules{}
-
 	filetype := rules.GetFileType(filename)
+	rules.addQuoteRules(filetype)
+	rules.addCommentRules(filetype)
+	rules.addMiscRules(filetype)
+	rules.addWhitespaceRules(filetype)
+	return &rules
+}
 
-	// Filetype specific rules
+func (rules *SyntaxRules) addQuoteRules(filetype string) {
 	switch filetype {
 	case "sh", "py", "yaml", "js", "c", "coffee", "html", "rb":
 		rules.addSingleQuoteRule(termbox.ColorYellow)
@@ -32,6 +38,9 @@ func NewSyntaxRules(filename string) *SyntaxRules {
 		rules.addSingleQuoteRule(termbox.ColorRed)
 		rules.addDoubleQuoteRule(termbox.ColorYellow)
 	}
+}
+
+func (rules *SyntaxRules) addCommentRules(filetype string) {
 	switch filetype {
 	case "go":
 		rules.addLineCommentRule("//", termbox.ColorCyan)
@@ -39,21 +48,24 @@ func NewSyntaxRules(filename string) *SyntaxRules {
 		rules.addLineCommentRule("#", termbox.ColorCyan)
 	case "c":
 		rules.addLineCommentRule("//", termbox.ColorCyan)
-	case "md":
-		rules.addRule("^#+.*$", Color{fg: termbox.ColorGreen})
-		rules.addRule("^===*$", Color{fg: termbox.ColorGreen})
-		rules.addRule("^---*$", Color{fg: termbox.ColorGreen})
 	case "js":
 		rules.addLineCommentRule("//", termbox.ColorCyan)
 	case "html":
 		rules.addRule("<!--.*?-->", Color{fg: termbox.ColorCyan})
 	}
+}
 
-	// Trailing whitespace
+func (rules *SyntaxRules) addMiscRules(filetype string) {
+	switch filetype {
+	case "md":
+		rules.addRule("^#+.*$", Color{fg: termbox.ColorGreen})
+		rules.addRule("^===*$", Color{fg: termbox.ColorGreen})
+		rules.addRule("^---*$", Color{fg: termbox.ColorGreen})
+	}
+}
+
+func (rules *SyntaxRules) addWhitespaceRules(filetype string) {
 	rules.addRule("[ \t]+$", Color{bg: termbox.ColorYellow})
-
-	return &rules
-
 }
 
 func (rules *SyntaxRules) addRule(reStr string, color Color) {
@@ -74,7 +86,23 @@ func (rules *SyntaxRules) addDoubleQuoteRule(fg termbox.Attribute) {
 	rules.addRule("\".*?\"", Color{fg: fg})
 }
 
+// GetFileType maps common extensions onto a type (e.g. c, C, cpp, etc
+// onto 'c'), but otherwise just returns the extension.
 func (rules SyntaxRules) GetFileType(filename string) string {
+	ext := rules.getFileExt(filename)
+	switch ext {
+	case "c", "C", "cpp", "c++":
+		return "c"
+	case "sh", "csh":
+		return "sh"
+	case "":
+		return ""
+	default:
+		return ext
+	}
+}
+
+func (rules SyntaxRules) getFileExt(filename string) string {
 	ext := path.Ext(filename)
 	if len(ext) == 0 {
 		basename := path.Base(filename)
@@ -84,26 +112,17 @@ func (rules SyntaxRules) GetFileType(filename string) string {
 		return ""
 	}
 	ext = ext[1:]
-	switch ext {
-	case ".c", ".C", ".cpp", ".c++":
-		return "c"
-	case ".sh", ".csh":
-		return "sh"
-	case "":
-		return ""
-	default:
-		return ext
-	}
+	return ext
 }
 
+// LineColor object maps a color pair (bg, fg) with start/end indices
+// within a string.
 type LineColor struct {
 	Fg, Bg     termbox.Attribute
 	Start, End int
 }
 
 // Colorize takes in a string and outputs an array of LineColor objects.
-// A linecolor object maps a color pair (bg, fg) with start/end indices
-// within the string.
 func (rules SyntaxRules) Colorize(str string) []LineColor {
 	lc := []LineColor{}
 	for _, rule := range rules {
---
+++ syntaxcolor/syntaxcolor_test.go
@@ -0,0 +1,37 @@
+package syntaxcolor_test
+
+import "github.com/wx13/sith/syntaxcolor"
+import "testing"
+import "fmt"
+
+func TestGetFileType(t *testing.T) {
+
+	sr := syntaxcolor.NewSyntaxRules("foo.go")
+
+	ft := sr.GetFileType("foo.go")
+	if ft != "go" {
+		t.Error("Error getting file type. Expected 'go', got", ft)
+	}
+
+	ft = sr.GetFileType("foo.cpp")
+	if ft != "c" {
+		t.Error("Error getting file type. Expected 'c', got", ft)
+	}
+
+}
+
+func ExampleColorize() {
+
+	sr := syntaxcolor.NewSyntaxRules("foo.go")
+
+	lc := sr.Colorize("package main")
+	fmt.Println(lc)
+
+	lc = sr.Colorize("var x ")
+	fmt.Println(lc[0].Start, lc[0].End)
+
+	// Output:
+	// []
+	// 5 6
+
+}
