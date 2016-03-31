7914157499c88394211503528762a0c79c1f517b
Jason DeVita <jason@wx13.com>
2016-03-30 22:51:21 -0700

    Change undo/redo history to linked list
    This will allow me to incorporate a saved snapshot
    history.
    
    	modified:   file/bufferhist.go

--- file/bufferhist.go
+++ file/bufferhist.go
@@ -1,11 +1,25 @@
 package file
 
-import "time"
+import (
+	"container/list"
+	"time"
+)
+
+type BufferState struct {
+	buff Buffer
+	mc   MultiCursor
+}
+
+func NewBufferState(buff Buffer, mc MultiCursor) *BufferState {
+	return &BufferState{
+		buff: buff,
+		mc:   mc,
+	}
+}
 
 type BufferHist struct {
-	buffers  []Buffer
-	cursors  []MultiCursor
-	idx      int
+	list     *list.List
+	element  *list.Element
 	snapChan chan struct{}
 	snapReq  SnapshotRequest
 }
@@ -17,8 +31,9 @@ type SnapshotRequest struct {
 
 func NewBufferHist(buffer Buffer, cursor MultiCursor) *BufferHist {
 	bh := BufferHist{}
-	bh.buffers = append(bh.buffers, buffer)
-	bh.cursors = append(bh.cursors, cursor.Dup())
+	state := NewBufferState(buffer, cursor)
+	bh.list = list.New()
+	bh.element = bh.list.PushBack(state)
 	bh.snapChan = make(chan struct{}, 1)
 	bh.handleSnapshots()
 	return &bh
@@ -30,8 +45,8 @@ func (bh *BufferHist) ForceSnapshot(buffer Buffer, mc MultiCursor) {
 
 func (bh *BufferHist) Snapshot(buffer Buffer, mc MultiCursor) {
 	request := SnapshotRequest{
-		Buffer: buffer,
-		Cursor: mc,
+		Buffer: buffer.Dup(),
+		Cursor: mc.Dup(),
 	}
 	bh.snapReq = request
 	select {
@@ -53,63 +68,32 @@ func (bh *BufferHist) handleSnapshots() {
 
 func (bh *BufferHist) snapshot(buffer Buffer, mc MultiCursor) {
 
-	var buffers []Buffer
-	var cursors []MultiCursor
-
-	dist := bh.cursors[bh.idx][0].row - mc[0].row
-	if bh.idx < len(bh.buffers) && (dist < -1 || dist > 1) {
-		bh.idx = bh.idx + 1
-
-		buffers = append(bh.buffers[:bh.idx], bh.buffers[bh.idx-1].Dup())
-		bh.buffers = append(buffers, bh.buffers[bh.idx:]...)
-
-		cursors = append(bh.cursors[:bh.idx], mc.Dup())
-		bh.cursors = append(cursors, bh.cursors[bh.idx:]...)
-	}
-
-	bh.idx = bh.idx + 1
-
-	buffers = append(bh.buffers[:bh.idx], buffer.Dup())
-	bh.buffers = append(buffers, bh.buffers[bh.idx:]...)
-
-	cursors = append(bh.cursors[:bh.idx], mc.Dup())
-	bh.cursors = append(cursors, bh.cursors[bh.idx:]...)
-
-	bh.Trim(200)
+	state := NewBufferState(buffer, mc)
+	bh.element = bh.list.InsertAfter(state, bh.element)
 
 }
 
 func (bh *BufferHist) Trim(n int) {
-	if bh.idx+n < len(bh.buffers) {
-		bh.buffers = bh.buffers[:(bh.idx + n)]
-		bh.cursors = bh.cursors[:(bh.idx + n)]
-	}
-	if bh.idx >= n {
-		bh.buffers = bh.buffers[(bh.idx - n):]
-		bh.cursors = bh.cursors[(bh.idx - n):]
-		bh.idx -= bh.idx - n
-	}
 }
 
 func (bh *BufferHist) Current() (Buffer, MultiCursor) {
-	return bh.buffers[bh.idx], bh.cursors[bh.idx].Dup()
+	state := bh.element.Value.(*BufferState)
+	return state.buff, state.mc.Dup()
 }
 
 func (bh *BufferHist) Next() (Buffer, MultiCursor) {
-	return bh.Increment(1)
-}
-
-func (bh *BufferHist) Prev() (Buffer, MultiCursor) {
-	return bh.Increment(-1)
-}
-
-func (bh *BufferHist) Increment(n int) (Buffer, MultiCursor) {
-	bh.idx += n
-	if bh.idx >= len(bh.buffers) {
-		bh.idx = len(bh.buffers) - 1
-	}
-	if bh.idx < 0 {
-		bh.idx = 0
+	next := bh.element.Next()
+	if next != nil {
+		bh.element = next
 	}
 	return bh.Current()
 }
+
+func (bh *BufferHist) Prev() (Buffer, MultiCursor) {
+	prev := bh.element.Prev()
+	if prev != nil {
+		bh.element = prev
+	}
+	return bh.Current()
+}
+
