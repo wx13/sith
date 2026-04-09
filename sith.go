f716bb7be15f75f8c82a3c84c396294215819e71
Jason DeVita <jason.devita@ionq.co>
2026-04-09 15:11:30 -0700

    print version number

--- sith.go
+++ sith.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"runtime/debug"
 
 	"github.com/wx13/sith/editor"
 )
 
+func printVersion() {
+	version := "(unknown)"
+	if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "" {
+		version = info.Main.Version
+	}
+	fmt.Println("sith", version)
+}
+
 func main() {
+	if len(os.Args) > 1 && (os.Args[1] == "-v" || os.Args[1] == "--version") {
+		printVersion()
+		return
+	}
 
 	session := editor.NewEditor()
 	defer session.Quit()
@@ -16,5 +30,4 @@ func main() {
 	session.Flush()
 	session.KeepFlushed()
 	session.Listen()
-
 }
